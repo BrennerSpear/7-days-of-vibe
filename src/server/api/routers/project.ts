1b402754438cee5bcfc3b1fd5c3041ab24e8cc39
Brenner Spear <blspear@gmail.com>
2025-03-18 17:36:23 -0400

    default true for now

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -2,36 +2,37 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const projectRouter = createTRPCRouter({
-  create: publicProcedure
-    .input(
-      z.object({
-        title: z.string().min(3).max(100),
-        description: z.string().min(10).max(500),
-        link: z.string().url(),
-        imageUrl: z.string().url(),
-        farcasterUsername: z.string().optional(),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      return ctx.db.project.create({
-        data: {
-          title: input.title,
-          description: input.description,
-          link: input.link,
-          imageUrl: input.imageUrl,
-          farcasterUsername: input.farcasterUsername ?? "", // Provide empty string as default
-        },
-      });
-    }),
+	create: publicProcedure
+		.input(
+			z.object({
+				title: z.string().min(3).max(100),
+				description: z.string().min(10).max(500),
+				link: z.string().url(),
+				imageUrl: z.string().url(),
+				farcasterUsername: z.string().optional(),
+			}),
+		)
+		.mutation(async ({ ctx, input }) => {
+			return ctx.db.project.create({
+				data: {
+					title: input.title,
+					description: input.description,
+					link: input.link,
+					imageUrl: input.imageUrl,
+					farcasterUsername: input.farcasterUsername ?? "", // Provide empty string as default
+					approved: true, // default to true for now
+				},
+			});
+		}),
 
-  getApproved: publicProcedure.query(({ ctx }) => {
-    return ctx.db.project.findMany({
-      where: {
-        approved: true,
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-  }),
+	getApproved: publicProcedure.query(({ ctx }) => {
+		return ctx.db.project.findMany({
+			where: {
+				approved: true,
+			},
+			orderBy: {
+				createdAt: "desc",
+			},
+		});
+	}),
 });
